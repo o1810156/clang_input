c4fb6e7bc95bd54cde1393d17dd364e4855bec69
anotherhollow1125 <anotherhollow1125@gmail.com>
2020-06-12 17:19:14 +0900

    C++でも使えるようにしたぱーと2

--- input.h
+++ input.h
@@ -1,13 +1,14 @@
 #ifndef _INCLUDE_input_h_
 #define _INCLUDE_input_h_
 
-#ifdef __cplusplus
+// GPPはg++でコンパイルするときに手動設定するものです。
+#if defined(__cplusplus) && !defined(GPP)
 extern "C" {
 #endif
 
 extern char* input(char *desc, ...);
 
-#ifdef __cplusplus
+#if defined(__cplusplus) && !defined(GPP)
 }
 #endif
 
